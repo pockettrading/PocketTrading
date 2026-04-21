8597ce6ac55d04a833fb8c9230498abfb2ab0246
pockettrading <rastaw6595@gmail.com>
2026-04-21 16:41:34 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBxbQCmKXpn1WYBlfOvtUlV51Qeb6GNHVg",
+  authDomain: "pockettrading-75755.firebaseapp.com",
+  projectId: "pockettrading-75755",
+  storageBucket: "pockettrading-75755.firebasestorage.app",
+  messagingSenderId: "351795883193",
+  appId: "1:351795883193:web:231d4f74537396f7d9bf68"
 };
 
 const app = initializeApp(firebaseConfig);
