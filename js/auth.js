d07dc8e27a63c9420aa2f580bf184a6651c6f255
pockettrading <rastaw6595@gmail.com>
2026-04-22 19:07:20 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,38 +1,30 @@
 import { auth, db } from "./firebase-config.js";
 import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-import {
-  doc,
-  setDoc
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 window.register = async function () {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const type = document.getElementById("accountType").value;
+const email = email.value;
+const password = password.value;
+const type = accountType.value;
 
-  const balance = type === "demo" ? 10000 : 0;
+const balance = type === "demo" ? 10000 : 0;
 
-  const user = await createUserWithEmailAndPassword(auth, email, password);
+const user = await createUserWithEmailAndPassword(auth, email, password);
 
-  await setDoc(doc(db, "users", user.user.uid), {
-    email,
-    accountType: type,
-    balance
-  });
+await setDoc(doc(db, "users", user.user.uid), {
+email,
+accountType: type,
+balance
+});
 
-  alert("Registered!");
-  window.location.href = "dashboard.html";
+location.href = "dashboard.html";
 };
 
 window.login = async function () {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  await signInWithEmailAndPassword(auth, email, password);
-
-  window.location.href = "dashboard.html";
+await signInWithEmailAndPassword(auth, email.value, password.value);
+location.href = "dashboard.html";
 };
